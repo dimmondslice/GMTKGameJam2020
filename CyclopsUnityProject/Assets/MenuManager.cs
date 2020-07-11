1f7baad3d5bf6db50c2362c9dbd9427d01ff615e
Stephen H <boomstevo@gmail.com>
2020-07-11 00:03:38 -0700

    Making a Main Menu
    fuck up all the shit.

---
+++ CyclopsUnityProject/Assets/MenuManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuManager : MonoBehaviour
+{
+    public GameObject mainMenu, levelMenu, optionsMenu;
+    GameObject activeMenu;
+
+    private void Start()
+    {
+        mainMenu.SetActive(false);
+        levelMenu.SetActive(false);
+        optionsMenu.SetActive(false);
+
+        activeMenu = mainMenu;
+        ChangeMenu(activeMenu);
+    }
+    public void ChangeMenu(GameObject newMenu)
+    {
+        activeMenu.SetActive(false);
+        newMenu.SetActive(true);
+        activeMenu = newMenu;
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
