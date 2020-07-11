629f57311304332973e2a5311f95db339ae9c422
Spencer <spencerjohnson714@gmail.com>
2020-07-11 15:57:42 -0700

    ++eye lazer ++explosions

--- CyclopsUnityProject/Assets/Code/CyclopsPlayer.cs
+++ CyclopsUnityProject/Assets/Code/CyclopsPlayer.cs
@@ -18,7 +18,20 @@ public class CyclopsPlayer : MonoBehaviour
   public float m_dSplosionFalloffDist;
   public float m_dMaxSplosionKnockbackSpeed;
 
+  public float m_dEyeOpenMinSec;
+  public float m_dEyeCloseMinSec;
+
+  public GameObject m_dExposionPrefab;
+  public GameObject m_dLaserPrefab;
+  public float m_dExplosionFreqSec;
+  public float m_dExplosionStartUpTime;
+
+  // Reference Gameobjects in prefab
   public Transform m_rCameraTr;
+  public Transform m_drBlinkBlock;
+  public Transform m_drEar0;
+  public Transform m_drEar1;
+
 
   // Privates
   private Rigidbody m_rRB;
@@ -29,11 +42,22 @@ public class CyclopsPlayer : MonoBehaviour
 
   private Vector3 m_newVelw;
 
+  // Eyes
+  private bool m_bChangeEyeState_CorRunning = false;
+  private bool m_bEyesClosed = false;
+
+  // Laser
+  private bool m_bFireLaser_CoreRunning = false;
+
   void Start()
   {
     m_rRB = GetComponent<Rigidbody>();
 
     Cursor.lockState = CursorLockMode.Locked;
+
+    m_drBlinkBlock.gameObject.SetActive(m_bEyesClosed);
+    m_bEyesClosed = false;
+    StartCoroutine(FireLaser_Cor());
   }
 
   private void Update()
@@ -41,6 +65,19 @@ public class CyclopsPlayer : MonoBehaviour
     if (Input.GetButtonDown("Jump"))
     {
       OnJump();
+    }
+
+    if (!m_bChangeEyeState_CorRunning)
+    {
+      if (!m_bEyesClosed && Input.GetButton("Blink")) // close eyes
+      {
+        StartCoroutine(ChangeEyeState_Cor());
+      }
+      else if (m_bEyesClosed && !Input.GetButton("Blink")) // open eyes
+      {
+        StartCoroutine(ChangeEyeState_Cor());
+        StartCoroutine(FireLaser_Cor());
+      }
     }
   }
 
@@ -87,11 +124,22 @@ public class CyclopsPlayer : MonoBehaviour
     }
 
     // Camera Look X Rot
-    //if (Mathf.Abs(m_rCameraTr.localRotation.eulerAngles.x) < 80.0f)
+    //Matrix4x4 rot = new Matrix4x4();
+    //rot.SetColumn(1, Vector4(Mathf.Cos()))
+   // Vector3 maxFwdUp = Vector3. (m_rCameraTr.forward
+
+    float angle = Mathf.Acos(Vector3.Dot(transform.forward, m_rCameraTr.forward)) * Mathf.Rad2Deg; //direction vectors are unit length so only need arccos for angle
+
+
+    float signedAngle = Vector3.SignedAngle(transform.forward, m_rCameraTr.forward, transform.right);
+    //print(signedAngle);
+    float pitchDelta = -1 * Input.GetAxis("Mouse Y") * m_dLookPitchSensitivity * Mathf.Deg2Rad;
+
+   // if (Mathf.Sign(signedAngle) < 80.0f || Mathf.Sign(pitchDelta) == Mathf.Sign(signedAngle))
     {
-      float pitchDelta = -1 * Input.GetAxis("Mouse Y") * m_dLookPitchSensitivity * Mathf.Deg2Rad;
       m_rCameraTr.RotateAround(m_rCameraTr.position, m_rCameraTr.right, pitchDelta);
     }
+ 
   }
 
   ////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -122,5 +170,44 @@ public class CyclopsPlayer : MonoBehaviour
       m_rRB.velocity = m_newVelw;
 
     }
+  }
+
+  ////////////////////////////////////////////////////////////////////////////////////////////////////
+  private IEnumerator ChangeEyeState_Cor()
+  {
+    m_bChangeEyeState_CorRunning = true;
+
+    m_bEyesClosed = !m_bEyesClosed;
+
+    m_drBlinkBlock.gameObject.SetActive(m_bEyesClosed);
+    float wait = m_bEyesClosed ? m_dEyeCloseMinSec : m_dEyeOpenMinSec;
+    yield return new WaitForSeconds(wait);
+
+    m_bChangeEyeState_CorRunning = false;
+  }
+
+  ////////////////////////////////////////////////////////////////////////////////////////////////////
+  private IEnumerator FireLaser_Cor()
+  {
+    m_bFireLaser_CoreRunning = true;
+
+    yield return new WaitForSeconds(m_dExplosionStartUpTime);
+
+    while (!m_bEyesClosed)
+    {
+      Ray ray = new Ray(m_rCameraTr.position, m_rCameraTr.forward);
+      Debug.DrawLine(ray.origin, ray.direction * 100.0f, Color.red);
+
+      if (Physics.Raycast(ray, out RaycastHit hitInfo, 100.0f))
+      {
+        Instantiate(m_dExposionPrefab, hitInfo.point, Quaternion.identity);
+
+        Instantiate(m_dLaserPrefab, m_drEar0.position, m_drEar0.transform.rotation, m_drEar0);
+        Instantiate(m_dLaserPrefab, m_drEar1.position, m_drEar1.transform.rotation, m_drEar1);
+      }
+
+      yield return new WaitForSeconds(m_dExplosionFreqSec);
+    }
+    m_bFireLaser_CoreRunning = false;
   }
 }
---
+++ CyclopsUnityProject/Assets/DestroyAfterSpawn.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyAfterSpawn : MonoBehaviour
+{
+  public float m_dDurationSec;
+
+  private float m_secSinceSpawn = 0.0f;
+
+  private void Update()
+  {
+    m_secSinceSpawn += Time.deltaTime;
+
+    if (m_secSinceSpawn > m_dDurationSec)
+    {
+      Destroy(gameObject);
+    }
+  }
+}
