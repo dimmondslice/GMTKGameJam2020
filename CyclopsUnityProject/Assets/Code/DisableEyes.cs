bbaf2bec6f221425ca6b144348f706abbd362cdb
Stephen H <boomstevo@gmail.com>
2020-07-11 23:48:32 -0700

    Safety Room

---
+++ CyclopsUnityProject/Assets/Code/DisableEyes.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisableEyes : MonoBehaviour
+{
+
+    private void OnTriggerEnter(Collider other)
+    {
+        CyclopsPlayer rPlayer = other.GetComponentInParent<CyclopsPlayer>();
+        if (rPlayer)
+        {
+            Debug.Log("THERE IS A PLAYER");
+            rPlayer.m_dSHOOTEYEBLASTS = false;
+            Destroy(gameObject);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
